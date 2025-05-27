f8b52690249376c0b802f9641b4c3f316304a3f8
luzvpascal <luz.pascal96@gmail.com>
2025-05-27 12:46:07 +1000

    models for plots with simulations

--- R/model_for_plots_logistic_growth.R
+++ R/model_for_plots_logistic_growth.R
@@ -2,19 +2,19 @@
 #' @description
 #' Simulate outputs of calibrated model, on data points only
 #' @param parameters vector of parameters (growth rate, capacity, initial abundance)
+#' @param simulation list of simulation arguments, as returned by \link[SMCfeatures]{model_logistic_growth_constraint}
 #' @param args a list of arguments as defined in \link[SMCfeatures]{SMCfeatures}
 #' @return ist like object representing simulation outputs
 #' t_data: time steps for data (args$input_data)
 #' y_data: output of simulation on input data
 #' @export
 model_for_plots_logistic_growth <- function(parameters,
+                                            simulation,
                                             args){
   # likelihood needs data at all time points
   #Inputs parameters and outputs a simulation
 
   # Simulation details
-  simulation <- list()
-
   simulation$t_plot <- seq(0,50)
   simulation$y_plot <- args$simulate_model(parameters,
                                            simulation$t_plot)
